430b3eb2b6e2eece87085c34446ff08ffdd55628
annaZakharchenko <anna.zakharchenko@student.karazin.ua>
2025-01-20 00:13:05 +0200

    test

---
+++ testJenkins/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the first number:");
+        int number1 = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number:");
+        int number2 = int.Parse(Console.ReadLine());
+
+        int sum = number1 + number2;
+
+        Console.WriteLine($"The sum is: {sum}");
+    }
+}
